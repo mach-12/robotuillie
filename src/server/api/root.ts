c79c62a4cec5e63dab4085f93b53b28090cc5f9a
mach-12 <mann.compi@gmail.com>
2023-12-10 03:39:40 +0530

    created new route and router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { rdbRouter } from "./routers/rdbRouter";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  reciepeDb: rdbRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/rdbRouter.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import { rdbProcedure, createTRPCRouter } from "../trpc";
+import { type SearchIngredientInRecipesResponse } from "../rdbUtils";
+
+export const rdbRouter = createTRPCRouter({
+  SearchIngredientInRecipes: rdbProcedure
+    .input(
+      z.object({
+        ingredient: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const myHeaders = new Headers();
+
+      myHeaders.append("Authorization", `Bearer ${ctx.response.access_token}`);
+
+      const res = await fetch(
+        `https://cosylab.iiitd.edu.in/api/recipeDB/searchingredientinrecipes/${input.ingredient}`,
+        {
+          method: "GET",
+          headers: myHeaders,
+          redirect: "follow",
+        },
+      );
+
+      const response =
+        (await res.json()) as SearchIngredientInRecipesResponse[];
+      return response;
+    }),
+});
