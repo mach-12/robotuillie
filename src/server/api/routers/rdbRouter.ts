e3b100ed7c10773077dbc2eb66a1251a8ca73b5b
mach-12 <mann.compi@gmail.com>
2023-12-10 04:44:33 +0530

    added weird shit

--- src/server/api/routers/rdbRouter.ts
+++ src/server/api/routers/rdbRouter.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 import { rdbProcedure, createTRPCRouter } from "../trpc";
-import { type ReceptorResponse, type SearchIngredientInRecipesResponse } from "../rdbUtils";
+import {
+  type ReceptorResponse,
+  type SearchIngredientInRecipesResponse,
+  getAccessToken,
+  addSecondsToCurrentTime,
+} from "../rdbUtils";
 
 export const rdbRouter = createTRPCRouter({
   SearchIngredientInRecipes: rdbProcedure
@@ -23,24 +28,97 @@ export const rdbRouter = createTRPCRouter({
         },
       );
 
-      const response =
-        (await res.json()) as SearchIngredientInRecipesResponse[];
-      return response;
+      try {
+        const response =
+          (await res.json()) as SearchIngredientInRecipesResponse[];
+
+        return response;
+      } catch (error) {
+        const token = await getAccessToken();
+        if (token) {
+          const myHeaders = new Headers();
+
+          myHeaders.append("Authorization", `Bearer ${token.access_token}`);
+
+          const res = await fetch(
+            `https://cosylab.iiitd.edu.in/api/recipeDB/searchingredientinrecipes/${input.ingredient}`,
+            {
+              method: "GET",
+              headers: myHeaders,
+              redirect: "follow",
+            },
+          );
+
+          await ctx.db.recipeDbToken.create({
+            data: {
+              access_token: token.access_token,
+              expires_on: addSecondsToCurrentTime(token.expires_in),
+              refresh_token: token.refresh_token,
+              rtoken_expires_on: addSecondsToCurrentTime(
+                token.refresh_expires_in,
+              ),
+              token_type: token.token_type,
+              id_token: token.id_token,
+            },
+          });
+
+          const response =
+            (await res.json()) as SearchIngredientInRecipesResponse[];
+
+          return response;
+        }
+      }
     }),
-    Receptors: rdbProcedure
-    .mutation(async({ctx})=>{
+  Receptors: rdbProcedure.mutation(async ({ ctx }) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${ctx.response.access_token}`);
 
+    const res = await fetch(
+      "https://cosylab.iiitd.edu.in/api/recipeDB/receptors",
+      {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      },
+    );
+
+    try {
+      const response = (await res.json()) as ReceptorResponse[];
+      return response;
+    } catch (error) {
+      const token = await getAccessToken();
+      if (token) {
         const myHeaders = new Headers();
-        myHeaders.append("Authorization", `Bearer ${ctx.response.access_token}`);
+        myHeaders.append(
+          "Authorization",
+          `Bearer ${ctx.response.access_token}`,
+        );
 
-        const res = await fetch("https://cosylab.iiitd.edu.in/api/recipeDB/receptors", {
-            method: 'GET',
+        const res = await fetch(
+          "https://cosylab.iiitd.edu.in/api/recipeDB/receptors",
+          {
+            method: "GET",
             headers: myHeaders,
-            redirect: 'follow'
-        })
+            redirect: "follow",
+          },
+        );
 
-        const response = await res.json() as ReceptorResponse[];
-        return response
-       
-    })
+        await ctx.db.recipeDbToken.create({
+          data: {
+            access_token: token.access_token,
+            expires_on: addSecondsToCurrentTime(token.expires_in),
+            refresh_token: token.refresh_token,
+            rtoken_expires_on: addSecondsToCurrentTime(
+              token.refresh_expires_in,
+            ),
+            token_type: token.token_type,
+            id_token: token.id_token,
+          },
+        });
+
+        const response = (await res.json()) as ReceptorResponse[];
+        return response;
+      }
+    }
+  }),
 });
